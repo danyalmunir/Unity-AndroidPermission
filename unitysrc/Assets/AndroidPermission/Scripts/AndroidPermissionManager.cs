6f9912812d48896b86420099b72e0d006e3caf6d
AkiSato <sato.geijo.13@gmail.com>
2017-06-26 22:56:06 +0900

    fix MIT

--- unitysrc/Assets/AndroidPermission/Scripts/AndroidPermissionManager.cs
+++ unitysrc/Assets/AndroidPermission/Scripts/AndroidPermissionManager.cs
@@ -2,7 +2,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2017 akisato.
+Copyright (c) 2017 sato0203.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
